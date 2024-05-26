f08da6f4e440764e3c1263ef704507d710c6556b
FelipeFTN <felipe.tenorio123456@gmail.com>
2024-05-26 01:14:05 -0300

    🧩 feat(web): getting completely beated by CSS

--- web/src/components/sidebar.rs
+++ web/src/components/sidebar.rs
@@ -7,9 +7,18 @@ pub fn SideBar() -> Element {
     rsx! {
         div { class: "sidebar",
             div { class: "profile",
-                img { src: "https://avatars.githubusercontent.com/u/80127749?v=4", alt: "Profile Picture"},
+                div { class: "profile-frame", 
+                    img { src: "https://avatars.githubusercontent.com/u/80127749?v=4", alt: "Profile Picture"},
+                }
                 h1 { "Felipe Tenório" },
                 p { "Software Developer" },
+                div { class: "social-links", 
+                    // I really need to align this shit better later.
+                    a { href: "https://github.com/FelipeFTN", 
+                        img { src: "github.svg", alt: "Github" }
+                        span { "@FelipeFTN" }
+                    },
+                }
             }
         }
     }
