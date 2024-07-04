3503b867ebe7e69758b80bbf06b74de9d42e7e08
FelipeFTN <felipe.tenorio123456@gmail.com>
2024-07-04 01:10:16 -0300

    🎮 feat(web): new game list

--- web/src/components/games.rs
+++ web/src/components/games.rs
@@ -2,21 +2,85 @@
 
 use dioxus::prelude::*;
 
+struct Game {
+    title: String,
+    cover: String,
+}
+
+fn get_games() -> Vec<Game> {
+    vec![
+        Game {
+            title: "Bioshock".to_string(),
+            cover: "games/bioshock.png".to_string(),
+        },
+        Game {
+            title: "Age of Empires II".to_string(),
+            cover: "games/age_of_empires_ii.png".to_string(),
+        },
+        Game {
+            title: "The Witcher".to_string(),
+            cover: "games/the_witcher.png".to_string(),
+        },
+        Game {
+            title: "Portal".to_string(),
+            cover: "games/portal.png".to_string(),
+        },
+        Game {
+            title: "Warcraft III".to_string(),
+            cover: "games/warcraft_iii.png".to_string(),
+        },
+        Game {
+            title: "Fallout 3".to_string(),
+            cover: "games/fallout.png".to_string(),
+        },
+        Game {
+            title: "Grim Fandango".to_string(),
+            cover: "games/grim_fandango.png".to_string(),
+        },
+        Game {
+            title: "Cyberpunk 2077".to_string(),
+            // cover: "games/cyberpunk_2077.png".to_string(),
+            cover: "https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/51f993d243a0d38e4669ac0bc3f8df05.webp".to_string(),
+        },
+        Game {
+            title: "Half-Life".to_string(),
+            cover: "games/half_life.png".to_string(),
+        },
+        Game {
+            title: "The Elder Scrolls IV: Oblivion".to_string(),
+            cover: "games/the_elder_scrolls_oblivion.png".to_string(),
+        },
+    ]
+}
+
+
 #[component]
 pub fn Games() -> Element {
     rsx! {
         div { class: "games",
             h1 { class: "title", "Games" }
             p { class: "subtitle", "Top best games I have played..." }
-        }
+
+            div { class: "games-list",
+                {
+                    get_games().iter().map(|game| rsx!{
+                        GameItem { title: game.title.clone(), cover: game.cover.clone() }
+                    }).collect::<Vec<_>>().into_iter()
+                }
+            }
+        },
     }
 }
 
-fn Game() -> Element {
+#[component]
+fn GameItem(title: String, cover: String) -> Element {
     rsx! {
         div { class: "game",
-            h2 { class: "title", "Game Title" },
-            p { class: "content", "Game Content" }
+            div { class: "game-wrapper",
+                // h2 { class: "title", title },
+                img { class: "cover", src: cover }
+                div { class: "gradient" }
+            }
         }
     }
 }
